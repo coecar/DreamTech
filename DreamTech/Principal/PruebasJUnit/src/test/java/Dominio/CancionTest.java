1cfc34e63c013b8f5196090540f605d74438a652
miguelgj17 <miguelangel.garcia31@alu.uclm.es>
2017-12-24 13:35:33 +0100

    Clases de Test

--- DreamTech/Principal/PruebasJUnit/src/test/java/Dominio/CancionTest.java
+++ DreamTech/Principal/PruebasJUnit/src/test/java/Dominio/CancionTest.java
@@ -1,59 +1,38 @@
 package Dominio;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
 import Persistencia.GestorCancionesTest;
 import Persistencia.GestorUsuariosTest;
 
 public class CancionTest {
+	private Cancion c;
 	protected String nombre;
 	protected boolean comprada;
 	protected int precio;
-	protected GestorCancionesTest gestorCanciones;
-	
-	public CancionTest (String nombre, boolean comprada, int precio){
-		this.nombre=nombre;
-		this.comprada=comprada;
-		this.precio=precio;
-		gestorCanciones=new GestorCancionesTest();
-	}
 
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public boolean isComprada() {
-		return comprada;
-	}
-
-	public void setComprada(boolean comprada) {
-		this.comprada = comprada;
-	}
-
-	public GestorCancionesTest getGestorCanciones() {
-		return gestorCanciones;
-	}
-
-	public void setGestorCanciones(GestorCancionesTest gestorCanciones) {
-		this.gestorCanciones = gestorCanciones;
-	}
-
-	
-	
-	public int getPrecio() {
-		return precio;
-	}
-
-	public void setPrecio(int precio) {
-		this.precio = precio;
-	}
-
-	@Override
-	public String toString() {
-		return "Cancion [nombre=" + nombre + ", comprada=" + comprada + "]";
+	@Before
+	public void setUp() throws Exception {
+		c = new Cancion(nombre, comprada, precio);
 	}
 	
+	@Test
+	public void testNombreIgual() {
+		assertEquals(nombre, c.getNombre());
+	}
 	
+	@Test
+	public void testComprada(){
+		assertTrue(!c.isComprada());
+		assertEquals(c.isComprada(), comprada);
+		
+	}
+	
+	@Test
+	public void testPrecioIgual(){
+		assertEquals(c.getPrecio(), precio);
+	}
 }
--- DreamTech/Principal/PruebasJUnit/src/test/java/Dominio/UsuarioTest.java
+++ DreamTech/Principal/PruebasJUnit/src/test/java/Dominio/UsuarioTest.java
@@ -1,63 +1,49 @@
 package Dominio;
 
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import Persistencia.GestorUsuarios;
 import Persistencia.GestorUsuariosTest;
+import Presentacion.Login;
 
 public class UsuarioTest {
+	
+	private Usuario u;
+	private GestorUsuarios gu = new GestorUsuarios();
 	protected String Nick;
 	protected String Password;
 	protected int saldo;
-	protected GestorUsuariosTest gestorUsuarios;
 	
-	public UsuarioTest (String Nick, String Password, int saldo){
-		this.Nick=Nick;
-		this.Password=Password;
-		this.saldo=saldo;
-		gestorUsuarios = new GestorUsuariosTest();
-	}
-
-	
-	
-	public int getSaldo() {
-		return saldo;
-	}
-
-	public void setSaldo(int saldo) {
-		this.saldo = saldo;
-	}
-
-
-
-	public String getNick() {
-		return Nick;
-	}
-
-	public void setNick(String nick) {
-		Nick = nick;
-	}
-
-	public String getPassword() {
-		return Password;
-	}
-
-	public void setPassword(String password) {
-		Password = password;
-	}
-
-	public GestorUsuariosTest getGestorUsuarios() {
-		return gestorUsuarios;
+	@Before
+	public void setUp() throws Exception {
+		u = new Usuario(Nick, Password, saldo);
 	}
 	
-
-	public void setGestorUsuarios(GestorUsuariosTest gestorUsuarios) {
-		this.gestorUsuarios = gestorUsuarios;
-	}
-
-	@Override
-	public String toString() {
-		return "Usuario [Nick=" + Nick + ", Password=" + Password + "]";
+	@Test
+	public void testNickIgual(){
+		assertEquals(Nick, u.getNick());
+		
 	}
 	
-	
-	
-	
+	@Test
+	public void testSaldo() throws IOException{
+		assertTrue(saldo==0);
+		/*Usuario [] usuarios = gu.leerUsuarios();
+		for(int i = 0; i<= usuarios.length; i++){
+			assertTrue(usuarios[i].getSaldo()>0);
+		}*/
+		
+		
+	}
+	@Test
+	public void testPasswordIgual(){
+		assertEquals(u.getPassword(), Password);
+	}
+
+
 }
